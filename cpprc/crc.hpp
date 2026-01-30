64078e421f92ba4b834b56de7167841908c5a75c
Kovacsics Robert <kovirobi@gmail.com>
2026-01-30 23:17:06 +0000

    Implement bitswap helper

--- cpprc/crc.hpp
+++ cpprc/crc.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
+#include <bit>
 #include <cstdint>
+#include <limits>
 
 namespace Crc
 {
     namespace Detail
     {
-        template<size_t width> struct UintN;
-        template<> struct UintN<8>  { using T = uint8_t;  };
-        template<> struct UintN<16> { using T = uint16_t; };
-        template<> struct UintN<32> { using T = uint32_t; };
-        template<> struct UintN<64> { using T = uint64_t; };
+        // Size-templated types ================================================
+
+        template<uint8_t width> struct UintN;
+        template<> struct UintN<8>  { using Type = uint8_t;  };
+        template<> struct UintN<16> { using Type = uint16_t; };
+        template<> struct UintN<32> { using Type = uint32_t; };
+        template<> struct UintN<64> { using Type = uint64_t; };
 
         // Rounds up to the nearest multiple of 8
         constexpr uint8_t roundUpTo8N(uint8_t x)
@@ -19,7 +23,52 @@ namespace Crc
         }
 
         // Minimal uintX_t which can store up to N
-        template<size_t N>
-        using Uint = UintN<roundUpTo8N(N)>;
+        template<uint8_t N>
+        using Uint = UintN<roundUpTo8N(std::bit_ceil(N))>::Type;
+
+        // Bitshift ============================================================
+
+        // A mask like 0x00FF/0x0F0F/0x3333/0x5555 used for bitswap
+        template<uint8_t N>
+        constexpr Uint<N> bitswapMask(uint8_t digits)
+        {
+            Uint<N> mask = (1 << digits) - 1;
+            Uint<N> oldMask = 0;
+            while (mask != oldMask)
+            {
+                oldMask = mask;
+                const auto gap = mask << digits;
+                mask = mask | (gap << digits);
+            }
+            return mask;
+        }
+
+        template<uint8_t N>
+        constexpr Uint<N> bitswap(Uint<N> value)
+        {
+            constexpr uint8_t digits = std::numeric_limits<Uint<N>>::digits;
+            for (uint8_t digit = digits >> 1; digit != 0; digit = digit >> 1)
+            {
+                const Uint<N> rmask = bitswapMask<N>(digit);
+                const Uint<N> lmask = rmask << digit;
+                value = ((value & lmask) >> digit) | ((value & rmask) << digit);
+            }
+            return value;
+        }
+
+        // Cyclic Redundancy Check (table driven/Sarwate algorithm) ===========
+
+        enum Bitorder
+        {
+            Msb,
+            Lsb,
+        };
+
+        template<uint8_t width, Uint<width> polynomial, Bitorder bitorder>
+        struct Impl
+        {
+            static constexpr auto poly = bitorder == Msb ? polynomial : bitswap(polynomial);
+            Uint<width> checksum;
+        };
     };
 };
--- test/test.hpp
+++ test/test.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "cpprc/crc.hpp"
+
+#include <concepts>
+#include <cstdint>
+
+using namespace Crc::Detail;
+
+static_assert(std::same_as<Uint<0>,  uint8_t>);
+static_assert(std::same_as<Uint<8>,  uint8_t>);
+static_assert(std::same_as<Uint<9>,  uint16_t>);
+static_assert(std::same_as<Uint<16>, uint16_t>);
+static_assert(std::same_as<Uint<17>, uint32_t>);
+static_assert(std::same_as<Uint<32>, uint32_t>);
+static_assert(std::same_as<Uint<33>, uint64_t>);
+static_assert(std::same_as<Uint<64>, uint64_t>);
+
+static_assert(bitswapMask<16>(8) == 0x00FF);
+static_assert(bitswapMask<16>(4) == 0x0F0F);
+static_assert(bitswapMask<16>(2) == 0x3333);
+static_assert(bitswapMask<16>(1) == 0x5555);
+
+static_assert(bitswap<32>(0x0000'0001) == 0x8000'0000);
+static_assert(bitswap<32>(0x1234'5678) == 0x1E6A'2C48);
